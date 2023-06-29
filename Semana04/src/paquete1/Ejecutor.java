cbf479dbe85ac4c5d3e6be1ee5f5cc7b26cf3ce9
Maria <mpmontano1@utpl.edu.ec>
2023-06-29 16:52:43 -0500

    Terminado

---
+++ Semana04/src/paquete1/Ejecutor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paquete1;
+
+
+
+/**
+ *
+ * @author SALA I
+ */
+public class Ejecutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Nodo bt = new Nodo(0);
+        
+        bt.add(8);
+        bt.add(6);
+        bt.add(7);
+        bt.add(5);
+
+
+        
+    }
+    
+}
---
+++ Semana04/src/paquete1/Nodo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete1;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Nodo {
+    int dato;
+    Nodo izquierda;
+    Nodo derecho;
+    Nodo raiz;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        izquierda = null;
+        derecho = null;
+    }
+    
+    
+}
